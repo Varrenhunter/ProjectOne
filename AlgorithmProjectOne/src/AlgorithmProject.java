40a59c51bcdf26aaba517a3ed723ed1624735918
Austin Suhler <austinsuhler@gmail.com>
2017-03-03 12:41:40 -0500

    Removed coments, streamlined code

--- AlgorithmProjectOne/src/AlgorithmProject.java
+++ AlgorithmProjectOne/src/AlgorithmProject.java
@@ -11,14 +11,7 @@ public class AlgorithmProject {
 	static int counter1 = 0;
 	static int counter2 = 0;
 	static int counter3 = 0;
-/* This code actually does nothing, it swaps the values locally only
-	public static boolean swap(int a, int b) {
-		int temp = 0;
-		temp = a;
-		a = b;
-		b = temp;
-		return false;
-	}*/
+
 	public static int[] swap(int a, int b){
 		int temp = 0;
 		temp = a;
@@ -41,7 +34,6 @@ public class AlgorithmProject {
 	 * Should be n!
 	 */
 	public static boolean ALG1(int[] A, int i) {
-		//counter3++;
 		if (i == A.length ) {
 			for (int j = 1; j < A.length - 1; j++) {
 				counter1++;
@@ -93,50 +85,21 @@ public class AlgorithmProject {
 		}
 		B[i] = 0;
 		
-		//counter2++;
-		 
 		ALG2(A, B, i + 1);
-		
-		//counter3++; 
-		 
+
 		B[i] = A[i];
 		ALG2(A, B, i + 1);
 		B[i] = 0;
 	}
 
 	public static void main(String args[]) {
-		/*
-		//populate A with random numbers
-		Random rand = new Random();
-		int arrayLength = 10;
-		int[] A = new int[arrayLength];
-		for(int j = 0; j<arrayLength; j++){
-			A[j] = rand.nextInt(49)+1;
-		}
-		System.out.print("I am array A: ");
-		for(int j = 0; j<A.length; j++){
-			System.out.print(A[j] + ", ");
-		}
-		System.out.println();
-		
-		//populate B with arrayLength values
-		int[] B = new int[arrayLength];
-		for(int j = 0; j<B.length; j++){
-			B[j] = 0;
-		}
-		for(int j = 0; j<B.length; j++){
-			System.out.print(B[j] + ", ");
-		}
-		System.out.println();
-		*/
+
 		int[] A = {9,8,7,6,5,4,3,2,1,0};//,4,3,2,1,0};
 		int[] B = {0,0,0,0,0,0,0,0,0,0};//,0,0,0,0};
 		int i = 0;
-		ALG2(A, B, i);
-		//ALG1(A,i);
+		//ALG2(A, B, i);
+		ALG1(A,i);
 		System.out.println("counter 1: " + counter1);
-		//System.out.println("counter 2: " + counter2);
-		//System.out.println("counter 3: " + counter3);
 		System.out.print("A: ");
 		for(int g = 0; g<A.length; g++){
 			System.out.print(A[g] + ", ");
